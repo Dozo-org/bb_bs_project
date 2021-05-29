f9c0e9e5534120046674428a0845e73e21ef647d
Avilov Oleg <dangerousmonk@yandex.ru>
2021-05-29 11:13:29 +0300

    Убрал @action, исправил get_queryset для manyToMany, исправил perform_create

--- events/views.py
+++ events/views.py
@@ -1,37 +1,38 @@
-from rest_framework import status
+from rest_framework import status, serializers
 from rest_framework.response import Response
 from .models import Event, EventParticipant
 from cities.models import Profile
 from .serializers import EventSerializer, EventParticipantSerializer
 from rest_framework.decorators import permission_classes, action
 from rest_framework import permissions, viewsets
+from django.shortcuts import get_object_or_404
 
 
-@action(detail=True, methods=['get'])
-class EventList(viewsets.ModelViewSet):
+class EventViewSet(viewsets.ModelViewSet):
+    serializer_class = EventSerializer
     allowed_methods = ['get', ]
 
     def get_queryset(self):
         if self.request.user.is_authenticated:
-            return Event.objects.filter(city=self.request.user.city).order_by(
-                'start_at')
+            return Event.objects.filter(city__in=self.request.user.city.all()).order_by('start_at')
         city_by_id = self.request.query_params.get('city')
         return Event.objects.filter(city=city_by_id).order_by('start_at')
 
-    serializer_class = EventSerializer
 
-
-@action(detail=True, methods=['get', 'post', 'delete'])
-@permission_classes([permissions.IsAuthenticated])
-class EventParticipantList(viewsets.ModelViewSet):
+class EventParticipantViewSet(viewsets.ModelViewSet):
     queryset = EventParticipant.objects.all()
     serializer_class = EventParticipantSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
-        event = Event.objects.get(id=self.kwargs['event'])
-        event.taken_seats += 1
-        event.save()
-        return serializer.save()
+        event_id = self.request.data.get('event')
+        event = get_object_or_404(Event, id=event_id)
+        if event.taken_seats < event.seats:
+            event.taken_seats += 1
+            event.save()
+            serializer.save(user=self.request.user)
+        else:
+            raise serializers.ValidationError({'seats': 'Нет доступных мест для регистрации'})
 
     def destroy(self, request, *args, **kwargs):
         event = Event.objects.get(id=self.kwargs['event'])
