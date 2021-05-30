534550e2d72fe7b942b1ccdc4291c7d8d175a3e3
Yauheni <smilodon2@yandex.ru>
2021-05-30 21:17:59 +0300

    добавил новый эндпоинт /api/v1/main добавил получение информации добавил выод результата запроса

--- events/urls.py
+++ events/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from .routers import CustomRouter
-from .views import EventViewSet, EventParticipantViewSet
+from .views import EventViewSet, EventParticipantViewSet, MainViewSet
 
 router = CustomRouter()
 router.register(r'events', EventViewSet, basename='event')
 router.register(r'event-participants', EventParticipantViewSet, basename='event-participant')
+router.register(r'main', MainViewSet, basename='main')
 
 
 urlpatterns = [
--- events/views.py
+++ events/views.py
@@ -1,11 +1,11 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions, viewsets
 from rest_framework import status, serializers
 from rest_framework.response import Response
+
+from .main_data import event
 from .models import Event, EventParticipant
-from cities.models import Profile
 from .serializers import EventSerializer, EventParticipantSerializer
-from rest_framework.decorators import permission_classes, action
-from rest_framework import permissions, viewsets
-from django.shortcuts import get_object_or_404
 
 
 class EventViewSet(viewsets.ModelViewSet):
@@ -14,7 +14,8 @@ class EventViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         if self.request.user.is_authenticated:
-            return Event.objects.filter(city__in=self.request.user.city.all()).order_by('start_at')
+            return Event.objects.filter(
+                city__in=self.request.user.city.all()).order_by('start_at')
         city_by_id = self.request.query_params.get('city')
         return Event.objects.filter(city=city_by_id).order_by('start_at')
 
@@ -38,10 +39,12 @@ class EventParticipantViewSet(viewsets.ModelViewSet):
             event.save()
             serializer.save(user=self.request.user)
         else:
-            raise serializers.ValidationError({'seats': 'Нет доступных мест для регистрации'})
+            raise serializers.ValidationError(
+                {'seats': 'Нет доступных мест для регистрации'})
 
     def get_object(self):
-        obj = get_object_or_404(self.get_queryset(), event=self.request.data.get('event'))
+        obj = get_object_or_404(self.get_queryset(),
+                                event=self.request.data.get('event'))
         self.check_object_permissions(self.request, obj)
         return obj
 
@@ -52,3 +55,21 @@ class EventParticipantViewSet(viewsets.ModelViewSet):
         event.save()
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class MainViewSet(viewsets.ModelViewSet):
+    serializer_class = EventSerializer
+    http_method_names = ['get']
+
+    def get_queryset(self):
+        return [Event.objects.all().latest('start_at')]
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+
+        response_data = {
+            'event': serializer.data[0],
+        }
+        response_data.update(event)
+        return Response(response_data)
