8c550a96a28b3d50738b744b824bf4c232a1eb6c
Yauheni <smilodon2@yandex.ru>
2021-06-03 01:03:44 +0300

    удалил docstring поправил код для соответствия новой структуры проекта

--- BBBS_project/urls.py
+++ BBBS_project/urls.py
@@ -1,30 +1,15 @@
-"""BBBS_project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
 
-from events.routers import CustomRouter
-from events.urls import router as event_router
-from cities.urls import router as cities_router
-from user.views import UsersViewSet
+from afisha.routers import CustomRouter
+from afisha.urls import router as afisha_router
+from common.urls import router as common_router
+from common.views import UsersViewSet
 
 v1_router = CustomRouter()
-v1_router.registry.extend(event_router.registry)
-v1_router.registry.extend(cities_router.registry)
+v1_router.registry.extend(afisha_router.registry)
+v1_router.registry.extend(common_router.registry)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
