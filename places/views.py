42671d18a484a18d0c3b20feef4afb691fd1bfa2
Avilov Oleg <dangerousmonk@yandex.ru>
2021-06-16 21:17:28 +0300

    Наброски функционала

--- places/views.py
+++ places/views.py
@@ -4,8 +4,16 @@ from rest_framework.decorators import action
 
 from .serializers import PlaceSerializer
 from .models import Place, Tag
+from common.models import Profile
 
 class PlacesViewSet(viewsets.ModelViewSet):
+    # TODO: filter by city, maybe default city?
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
     http_method_names = ['get']
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            profile = get_object_or_404(Profile, user=self.request.user)
+            return Place.objects.filter(city=profile.city)
+        return Place.objects.all()
