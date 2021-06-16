e74142693aa2c7517e2f998f4e26a97b0277cf34
dangerousmonk <dangerousmonk@yandex.ru>
2021-06-16 16:36:47 +0300

    Фикс: takenSeats теперь поле называется

--- afisha/views.py
+++ afisha/views.py
@@ -34,8 +34,8 @@ class EventParticipantViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         event_id = self.request.data.get('event')
         event = get_object_or_404(Event, id=event_id)
-        if event.taken_seats < event.seats:
-            event.taken_seats += 1
+        if event.takenSeats < event.seats:
+            event.takenSeats += 1
             event.save()
             serializer.save(user=self.request.user)
         else:
@@ -51,7 +51,7 @@ class EventParticipantViewSet(viewsets.ModelViewSet):
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         event = Event.objects.get(pk=instance.event.id)
-        event.taken_seats -= 1
+        event.takenSeats -= 1
         event.save()
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
